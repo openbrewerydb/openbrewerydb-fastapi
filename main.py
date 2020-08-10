22aeb3bf05545884ed5240a2663a929724de5981
Chris Mears <chris.mears@gmail.com>
2020-08-09 22:30:29 -0700

    chore: Update Github Actions workflow

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 from fastapi import FastAPI
-from fastapi.testclient import TestClient
 
 app = FastAPI()
-client = TestClient(app)
-
-#
-# ENDPOINTS
-#
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-#
-# TESTS
-#
-
-
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
