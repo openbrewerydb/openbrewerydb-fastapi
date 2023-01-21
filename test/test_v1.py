a6de1aee364380a644edbc38c368699d4f3d52c1
Miles <milsman2@gmail.com>
2023-01-21 23:17:25 +0000

    Endpoint testing.

--- test/test_v1.py
+++ test/test_v1.py
@@ -13,7 +13,38 @@ def test_v1_root():
     assert response.status_code == 200
     assert response.json() == {"version": 1}
 
-def test_breweries():
+
+def test_breweries_meta():
+    response = client.get("/v1/breweries/meta")
+    assert response.status_code == 200
+    assert response.json() == {"endpoint": "metadata"}
+
+
+def test_breweries_delete_id():
+    response = client.delete("/v1/breweries/1")
+    assert response.status_code == 501
+    assert response.json() == {"endpoint": "delete_brewery_by_id"}
+
+
+def test_post_breweries():
+    response = client.post("/v1/breweries")
+    assert response.status_code == 201
+    assert response.json() == {"endpoint": "post_breweries"}
+
+
+def test_get_brewery_by_id():
+    response = client.get("/v1/breweries/1")
+    assert response.status_code == 200
+    assert response.json() == {"endpoint": "get_brewery_by_id"}
+
+
+def test_get_random_brewery():
+    response = client.get("/v1/breweries/random")
+    assert response.status_code == 200
+    assert response.json() == {"endpoint": "get_random_brewery"}
+
+
+def test_get_all_breweries():
     response = client.get("/v1/breweries")
     assert response.status_code == 200
-    assert response.json() == {"endpoint": "breweries"}
+    assert response.json() == {"endpoint": "get_all_breweries"}
--- v1/routes/breweries.py
+++ v1/routes/breweries.py
@@ -7,9 +7,36 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/")
-async def endpoint():
-    """
-    Ping style health check
-    """
-    return {"endpoint": "breweries"}
+@router.get("/", status_code=200)
+async def get_all_breweries():
+    return {"endpoint": "get_all_breweries"}
+
+
+@router.post("/", status_code=201)
+async def post_breweries():
+    return {"endpoint": "post_breweries"}
+
+
+@router.get("/random/", status_code=200)
+async def get_random_brewery():
+    return {"endpoint": "get_random_brewery"}
+
+
+@router.get("/meta/", status_code=200)
+async def meta():
+    return {"endpoint": "metadata"}
+
+
+@router.delete("/{id}/", status_code=501)
+async def delete_brewery_by_id():
+    return {"endpoint": "delete_brewery_by_id"}
+
+
+@router.get("/{id}/", status_code=200)
+async def get_brewery_by_id():
+    return {"endpoint": "get_brewery_by_id"}
+
+
+@router.post("/{id}/", status_code=201)
+async def update_brewery_by_id():
+    return {"endpoint": "update_brewery_by_id"}
